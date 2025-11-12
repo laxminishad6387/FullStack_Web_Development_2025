1cf886053c9a60611524e3b97d672cf4047bd518
Laxmi Nishad <laxminishad00000@gmail.com.>
2025-11-12 14:28:53 +0530

    add new element

--- javaScript/Document_Object_Model/DOM_Method_2/script.js
+++ javaScript/Document_Object_Model/DOM_Method_2/script.js
@@ -1,4 +1,9 @@
 let ul=document.getElementById("myList");
 let newli=document.createElement("li");
 newli.textContent="Three";
-ul.appendChild(newli);
+ul.appendChild(newli);  // Append the new li to the ul
+
+let newDiv=document.createElement("div");
+newDiv.textContent="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit, possimus?"
+document.body.appendChild(newDiv) // Append the new div to the body
+newDiv.style.backgroundColor="lightblue";
