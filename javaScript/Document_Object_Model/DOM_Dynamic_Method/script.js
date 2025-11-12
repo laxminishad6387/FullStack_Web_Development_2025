13ee329cfb82dc8d8ea4f3fba6cf04c597dbb299
Laxmi Nishad <laxminishad00000@gmail.com.>
2025-11-12 15:48:31 +0530

    dynamic mehtod in js completed

---
+++ javaScript/Document_Object_Model/DOM_Dynamic_Method/script.js
@@ -0,0 +1,14 @@
+let ul=document.querySelector('ul');
+let li=document.createElement("li");
+li.textContent="Three";
+ul.appendChild(li);  // Append the new li to the ul
+
+//  we can also use other dynamic methods like  removeChild, replaceChild, etc.
+// replacing an element
+let newli=document.createElement("li");
+newli.textContent="new three";
+ul.replaceChild(newli,li); // Replace the old li with newli
+
+// Removing an element
+ul.removeChild(newli); // This will remove the newli from the ul
+
