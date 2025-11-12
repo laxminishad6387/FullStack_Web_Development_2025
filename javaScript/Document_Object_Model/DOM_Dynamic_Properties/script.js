cc14fea563aa7f91e3392626fcccbf6599f68456
Laxmi Nishad <laxminishad00000@gmail.com.>
2025-11-12 15:12:33 +0530

    Dynamic properties in js completed

---
+++ javaScript/Document_Object_Model/DOM_Dynamic_Properties/script.js
@@ -0,0 +1,6 @@
+let img=document.querySelector('img');
+console.log(img); 
+
+img.getAttribute('src'); // Gets the current value of the 'src' attribute 
+
+img.setAttribute('src',"https://as2.ftcdn.net/v2/jpg/05/70/00/45/1000_F_570004598_U8OFHVhDNFVs6qHXDhQd6hij4KHOd1Dq.jpg")   // Updates (or replaces) the 'src' attribute with a new image URL —// this changes the displayed image on the webpage
